fed0162f836293ce646b4a7bce60763840df1b03
Tudor <tbrindus@gmail.com>
2015-11-20 22:26:12 -0500

    Remove OpenGL acceleration; fails on some computers

--- src/main/java/tk/ivybits/automata/Main.java
+++ src/main/java/tk/ivybits/automata/Main.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String... argv) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
-        System.setProperty("sun.java2d.opengl", "true");
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         SwingUtilities.invokeLater(new Runnable() {
             @Override
